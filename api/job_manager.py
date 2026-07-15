9e40cc36a97a6462c895326b5241fa43277db7fd
SuperHero1st <ahmedsaidmeod@gmail.com>
2026-07-15 22:11:31 +0300

    Add files via upload

--- api/job_manager.py
+++ api/job_manager.py
@@ -202,6 +202,7 @@ class JobManager:
             voice_pitch=voice_pitch,
             voice_style=voice_style,
             clone_speaker=clone_speaker,
+            burn_subtitles=burn_subtitles,
             enable_lipsync=enable_lipsync,
         )
         job_id = job.job_id
